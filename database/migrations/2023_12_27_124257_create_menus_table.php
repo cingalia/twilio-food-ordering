21969c3835fb6584e79044e2044ce0ecdda97eeb
Clinton Ingalia <bicxtor1@gmail.com>
2023-12-27 15:46:08 +0300

    updated migration

--- database/migrations/2023_12_27_124257_create_menus_table.php
+++ database/migrations/2023_12_27_124257_create_menus_table.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('menus', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->string("name");
+            $table->string("price");
             $table->timestamps();
         });
     }
